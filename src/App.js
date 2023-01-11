4c7ada5dca5194656cd296edbf0f78aa0e2f556e
Leonardo Selegar <leonardo11132@gmail.com>
2023-01-10 23:38:19 -0300

    Removendo a pag contato email

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Habilidades from "./pages/Habilidades"
 import Projetos from './pages/Projetos';
 import Certificados from './pages/Certificados';
 import Footer from './components/Footer';
-import ContatoEmail from './pages/ContatoEmail';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -15,7 +14,6 @@ function App() {
       <Habilidades />
       <Projetos />
       <Certificados />
-      <ContatoEmail />
       <Footer />
     </div>
   );
